7cf8afc2776eaf1495c28fbe9279beb35d420d74
Marcos Vinícius <marcosvm1000@gmail.com>
2024-12-01 18:21:49 -0300

    Foram realizadas alterações na entidade material

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import { buildAuthenticatedRouter } from '@adminjs/express';
 
 import ColaboradorController from './controllers/colaborador.js';
+import MaterialController from './controllers/material.js';
 import provider from './admin/auth-provider.js';
 import options from './admin/options.js';
 
@@ -38,6 +39,9 @@ const start = async () => {
   app.use(admin.options.rootPath, router);
   app.use(bodyParser.json());
   app.use('/colaborador', ColaboradorController);
+
+  app.use('/material', MaterialController);
+
   app.listen(port, () => {
     console.log(`AdminJS available at http://localhost:${port}${admin.options.rootPath}`);
   });
---
+++ src/controllers/material.ts
@@ -0,0 +1,74 @@
+import { Router } from 'express';
+import MaterialService from 'src/services/material.js';
+
+const router = Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const { nome, categoria, unidade_medida, quantidade_minima, quantidade_atual } = req.body;
+
+    const material = await MaterialService.insert(nome, categoria, unidade_medida, quantidade_minima, quantidade_atual);
+    res.status(201).json(material);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const materiais = await MaterialService.getAll();
+    res.json(materiais);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const _id = Number(id);
+    if (Number.isNaN(_id) || !Number.isInteger(_id)) {
+      res.status(400).json({ message: 'Id inválido!' });
+    }
+    const material = await MaterialService.getById(_id);
+    res.json(material);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const _id = Number(id);
+    if (Number.isNaN(_id) || !Number.isInteger(_id)) {
+      res.status(400).json({ message: 'Id inválido!' });
+    }
+    await MaterialService.delete(_id);
+    res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { nome, categoria, unidade_medida, quantidade_minima, quantidade_atual } = req.body;
+    const { id } = req.params;
+    const _id = Number(id);
+    if (Number.isNaN(_id) || !Number.isInteger(_id)) {
+      res.status(400).json({ message: 'Id inválido!' });
+    }
+    const material = await MaterialService.update(_id, nome, categoria, unidade_medida, quantidade_minima, quantidade_atual );
+    res.json(material);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+export default router;
