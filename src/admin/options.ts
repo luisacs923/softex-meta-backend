316e79984a5b97a7521afaea7863d5825f0761fa
Andre De Carli <a.decarli89@gmail.com>
2024-12-02 15:15:36 -0300

    Configuração inicial do AdminJS

--- src/admin/options.ts
+++ src/admin/options.ts
@@ -1,5 +1,6 @@
 import { AdminJSOptions } from 'adminjs';
-// import prisma from 'src/db/index.js';
+import prisma from 'src/db/index.js';
+import { getModelByName } from '@adminjs/prisma';
 
 import componentLoader from './component-loader.js';
 
@@ -7,17 +8,94 @@ const options: AdminJSOptions = {
   componentLoader,
   rootPath: '/admin',
   resources: [
-    // {
-    //   resource: prisma.colaborador, // Referência para o modelo 'Colaborador' do Prisma
-    //   options: {
-    //     listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
-    //     editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
-    //     filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
-    //   },
-    // },
-    //   // Adicione outros recursos aqui, se necessário
+    {
+      resource: { model: getModelByName('Colaborador'), client: prisma },
+      options: {
+        titleProperty: 'nome',
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('Servico'), client: prisma },
+      options: {
+        titleProperty: 'nome',
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('Prestador'), client: prisma },
+      options: {
+        titleProperty: 'nome',
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('Cliente'), client: prisma },
+      options: {
+        titleProperty: 'nome',
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('Material'), client: prisma },
+      options: {
+        titleProperty: 'nome',
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('EPI'), client: prisma },
+      options: {
+        titleProperty: 'nome',
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('EntregaEpi'), client: prisma },
+      options: {
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('ContratoGeral'), client: prisma },
+      options: {
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('ContratoColaborador'), client: prisma },
+      options: {
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
+    {
+      resource: { model: getModelByName('MaterialContrato'), client: prisma },
+      options: {
+        // listProperties: ['id', 'nome', 'cpf', 'cbo', 'entregaEpi', 'contrato_colaborador'], // Campos de listagem
+        // editProperties: ['nome', 'cpf', 'cbo'], // Campos editáveis
+        // filterProperties: ['nome', 'cpf', 'cbo'], // Campos para filtragem
+      },
+    },
   ],
-  databases: [],
+  // databases: [],
 };
 
 export default options;
